55c2f00923d9180a6372072d5927ac48e8d7271f
Graham Chester <chesters99@yahoo.com>
2016-01-09 14:51:56 +0000

    fix userprofile not being created

--- main/models.py
+++ main/models.py
@@ -1,15 +1,24 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils import timezone
 from django.db import models
 from eventlog.models import log
 from pytz import all_timezones
 from .countries import COUNTRIES
 from django.conf import settings
+from django.contrib.auth.models import User
 
 TIME_ZONE_CHOICES = [(tz, tz) for tz in all_timezones]
 
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.get_or_create(user=instance)
+
+
 class UserProfile(models.Model):
-    user = models.OneToOneField('auth.User', related_name='profile')
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE, related_name='profile')
     time_zone = models.CharField(max_length=50, blank=False, default=settings.TIME_ZONE, choices=TIME_ZONE_CHOICES)
     address_line1 = models.CharField(max_length=50, blank=True)
     address_line2 = models.CharField(max_length=50, blank=True)
@@ -18,6 +27,9 @@ class UserProfile(models.Model):
     post_code = models.CharField(max_length=10, blank=True)
     country = models.CharField(max_length=40, blank=False, choices=COUNTRIES, default='GB')
     stripe_customer_id = models.CharField(max_length=40, blank=True)
+
+    def __str__(self):
+        return '%s %s' % (self.user_id, self.country)
 
     def save(self, *args, **kwargs):
         self.time_zone = timezone.get_current_timezone_name()
