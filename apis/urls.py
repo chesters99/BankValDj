fbdd04c9ed7d51fcd010dc1aa126ae7fce128547
Graham Chester <chesters99@yahoo.com>
2016-02-18 14:38:31 +0000

    rework django rest framework to use Class Based Views

--- apis/urls.py
+++ apis/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from apis.views import validate, RuleViewSet, RuleList
-
+from apis.views import RuleViewSet, RuleList, Validate
 
 urlpatterns = [
-    url(r'^validate/$', validate, name='validate'),
+    url(r'^validate/$', Validate.as_view(), name='validate'),
     url(r'^rulelist/$', RuleList.as_view(), name='rulelist'),
     url(r'^rule/(?P<pk>\d+)$', RuleViewSet.as_view({'get':'retrieve', 'put':'update', 'post':'create','delete':'destroy',}), name='rule'),
 ]
--- apis/views.py
+++ apis/views.py
@@ -1,12 +1,10 @@
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import serializers, status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-
 from accounts.utils.BankValidator import Validator
 from rules.models import Rule
 
@@ -19,22 +17,24 @@ class RuleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rule
 
-@csrf_exempt
-@api_view(['POST'])
-@permission_classes((AllowAny,))
-def validate(request):  # pseudo-RESTful API
-    serializer = ValidateSerialiser(data=request.data)
-    if serializer.is_valid():
-        bv = Validator()
-        if bv.validate(sort_code=serializer.data['sort_code'],
-                       account_number=serializer.data['account_number']):
-            serializer.data['message'] = 'Valid Account'
-            return Response(serializer.data, status=status.HTTP_200_OK)
+
+class Validate(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = [AllowAny,]
+
+    def post(self, request):
+        serializer = ValidateSerialiser(data=request.data)
+        if serializer.is_valid():
+            bv = Validator()
+            if bv.validate(sort_code=serializer.data['sort_code'],
+                           account_number=serializer.data['account_number']):
+                serializer.data['message'] = 'Valid Account'
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                serializer.data['message'] = bv.message
+                return Response(serializer.data, status=status.HTTP_406_NOT_ACCEPTABLE)
         else:
-            serializer.data['message'] = bv.message
-            return Response(serializer.data, status=status.HTTP_406_NOT_ACCEPTABLE)
-    else:
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RuleViewSet(ModelViewSet):
