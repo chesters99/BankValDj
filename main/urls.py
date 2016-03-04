85bc0678a3ad97738d81e4303f9fa165d41176d1
Graham Chester <chesters99@yahoo.com>
2016-03-04 17:18:14 +0000

    imprived view caching

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login
-from django.views.generic import TemplateView
-from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
-    url(r'^about/$',   cache_page(60*15)(TemplateView.as_view(template_name='about.html')), name='about'),
-    url(r'^contact/$', cache_page(60*15)(TemplateView.as_view(template_name='contact.html')), name='contact'),
-    url(r'^showdoc/$', cache_page(60*15)(TemplateView.as_view(template_name='show_document.html')), name='showdoc'),
+    url(r'^about/$',   views.AboutView.as_view(), name='about'),
+    url(r'^contact/$', views.ContactView.as_view(), name='contact'),
+    url(r'^showdoc/$', views.DocumentView.as_view(), name='showdoc'),
     url(r'^createuser/$', views.CreateUser.as_view(), name='createuser'),
     url(r'^graph/$',      views.Graph.as_view(), name='graph'),
     url(r'^loginuser/$',  login, {'template_name': 'login.html'}, name='loginuser'),
