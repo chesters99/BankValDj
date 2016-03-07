3293853ca26389b11f45f063b8c6f84ddfa4dffe
Graham Chester <chesters99@yahoo.com>
2016-03-07 11:15:02 +0000

    enhance sphinx docs to enable upload to readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'BankValDj.settings.localvm'
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'BankValDj.settings.localvm'
 import django
 django.setup()
 # -- General configuration ------------------------------------------------
