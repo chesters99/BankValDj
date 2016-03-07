4364ae47cd71ca63f132366439c75ab7feba8088
Graham Chester <chesters99@yahoo.com>
2016-03-07 13:37:04 +0000

    move secrets from json file to environment variables

--- docs/conf.py
+++ docs/conf.py
@@ -17,10 +17,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-# import os
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'BankValDj.settings.localvm'
-# import django
-# django.setup()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
